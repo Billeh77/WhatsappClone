3ed6f5ceb62748fcad512b1e9da72593f77d7913
Billeh77 <billehemile@gmail.com>
2024-05-27 13:43:07 -0400

    Initial Commit

---
+++ Whatsapp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Whatsapp
+//
+//  Created by Emile Billeh on 27/05/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Whatsapp/WhatsappApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsappApp.swift
+//  Whatsapp
+//
+//  Created by Emile Billeh on 27/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct WhatsappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
